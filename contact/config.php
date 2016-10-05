7afa90f3655e1fcc630e0fa6fa67b8f83d9bd551
Lea <leandrogabrielgarcia@gmail.com>
2016-10-04 23:11:09 -0300

    arreglado el envio de mails

---
+++ contact/config.php
@@ -0,0 +1,4 @@
+<?php
+// To
+define("WEBMASTER_EMAIL", 'aliciatettamanti@yahoo.com.ar');
+?>
---
+++ contact/contact.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Credits: Bit Repository
+URL: http://www.bitrepository.com/
+*/
+
+include 'config.php';
+
+error_reporting (E_ALL ^ E_NOTICE);
+
+$post = (!empty($_POST)) ? true : false;
+
+if($post)
+{
+
+$name = stripslashes($_POST['name']);
+$email = trim($_POST['email']);
+$message = stripslashes($_POST['message']);
+
+
+$error = '';
+
+
+
+if(!$error)
+{
+$mail = mail(WEBMASTER_EMAIL,"Mail From Web", $message,
+     "From: ".$name." <".$email.">\r\n"
+    ."Reply-To: ".$email."\r\n"
+    ."X-Mailer: PHP/" . phpversion());
+
+
+if($mail)
+{
+echo 'OK';
+}
+
+}
+
+
+}
+?>
